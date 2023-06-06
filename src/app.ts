db577789bafd84fbbc9dc9174e3e5988329e8d76
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-05 20:33:49 -0400

    Use dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(4000, () => console.log("Funcionando por el puerto 4000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Funcionando por el puerto ${PORT}`));
