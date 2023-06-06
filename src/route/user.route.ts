449faaec3df8bd0e2e91846cbddb6cd7180b62c1
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-06 11:28:44 -0400

    Route of user

---
+++ src/route/user.route.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { getUserCtrl, insertUserCtrl } from "../controllers/user.ctrl";
+
+export const router = Router();
+
+/*
+ * Route https://[IpV4]:[PORT]/user
+ * Method Get
+ * */
+router.get('/', getUserCtrl);
+
+/*
+ * Route https://[IpV4]:[PORT]/user/register
+ * Method Post
+ * */
+router.post('/register', insertUserCtrl);
