c64e972dfd51732dedec5161c6e04ca8a13c22ee
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-06 11:27:10 -0400

    Use routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { router } from "./route";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 const PORT = process.env.PORT || 5000;
 
