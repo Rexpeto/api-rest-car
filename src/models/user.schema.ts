37441c35af26d66d88de18a603f782bf0da87a39
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-06 08:24:56 -0400

    Schema users

---
+++ src/models/user.schema.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+
+    }, {
+    timestamps: true
+});
+
+const UserModel = model("users", userSchema);
+
+export default UserModel;
