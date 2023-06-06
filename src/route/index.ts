07e89fb281337c5887b422c5b7557762c533c9d7
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-06 11:27:59 -0400

    import routes dinamyc

---
+++ src/route/index.ts
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { readdirSync } from 'fs';
+
+const PATH_ROUTE = `${__dirname}`;
+
+export const router = Router();
+
+/*
+ * Clean name of file 
+ * Example: user.route.ts -> user
+ * @param fileName
+ * @returns
+ * */
+const cleanFileName = (fileName: string) => {
+    return fileName.split('.').shift();
+};
+
+/*
+ * Check file in folder router and use router export
+ * @param fileName
+ * @returns
+ **/
+readdirSync(PATH_ROUTE).filter(fileName => {
+    const file = cleanFileName(fileName);
+
+    if (file !== 'index') {
+        import(`./${fileName}`).then((moduleRouter) => {
+            router.use(`/${file}`, moduleRouter.router);
+        });
+
+        console.log(`Ruta cargada -> ${file}`);
+    }
+});
