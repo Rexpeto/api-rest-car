976bc0fe8be7647998d764544a3228185346aed5
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-06 11:30:34 -0400

    Initial service of user

---
+++ src/services/user.service.ts
@@ -0,0 +1,27 @@
+import UserModel from "../models/user.schema";
+import { UserInterface } from "../types/user.type";
+
+/**
+ * Register user
+ * @param user
+ * @returns
+ * */
+
+export const registerUser = async (user: UserInterface) => {
+    const response = await UserModel.create(user);
+
+    return response;
+};
+
+/**
+ * Seach user by email
+ * @param email
+ * @returns
+ * */
+
+export const getUser = async (email: string) => {
+    const response = await UserModel.findOne({ email });
+
+    return response;
+};
+
