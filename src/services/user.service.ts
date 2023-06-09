fc12e9bd6f5e9bec2d7cc4a5161fb1a0bbba3889
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-09 01:31:41 -0400

    Update register user and use encript

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,7 @@
 import UserModel from "../models/user.schema";
+import AuthUser from "../types/authUser";
 import UserInterface from "../types/user.type";
+import { encrypt } from "../utils/bcrypt.handle";
 
 /**
  * Register user
@@ -7,18 +9,31 @@ import UserInterface from "../types/user.type";
  * @returns
  * */
 
-export const registerUser = async (user: UserInterface) => {
-    const response = await UserModel.create(user);
+export const registerUser = async ({name, email, password}: UserInterface) => {
+    const isCheck = await UserModel.findOne({ email });
 
-    return response;
+    if(isCheck) {
+        return <string> "El correo electronico ya existe"
+    }
+
+    //? Hash password of user
+    const passwordHash = await encrypt(password);
+
+    const newUser = await UserModel.create(
+        {
+            name: name.toLowerCase(),
+            email: email.toLowerCase(),
+            password: passwordHash
+        }
+    );
+
+    return newUser;
 };
 
 /**
- * Seach user by email
- * @param email
+ * Login user
+ * @param Auth
  * @returns
  * */
 
-export const getUser = async () => {
-    return { data: "Hola mundo ->" };
-};
+export const loginUser = async (Auth: AuthUser) => {};
