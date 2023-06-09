671b52f597b41e0dad9a36c0398009f9d2036974
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-09 00:19:03 -0400

    Controller of register user

--- src/controllers/user.ctrl.ts
+++ src/controllers/user.ctrl.ts
@@ -1,15 +1,44 @@
 import { type Request, type Response } from "express";
+import { getUser, registerUser } from "../services/user.service";
+import handleHttp from "../utils/error.handle";
 
 export const getUserCtrl = async (
     req: Request,
     res: Response
 ): Promise<void> => {
-    res.json({ msg: "Test from get" });
+    const response = getUser();
+    res.json({ msg: "Test from get ->" });
 };
 
-export const insertUserCtrl = async (
+export const getUsers = async (
     req: Request,
     res: Response
+): Promise<void> => {};
+
+export const insertUserCtrl = async (
+    { body }: Request,
+    res: Response
 ): Promise<void> => {
-    res.json({ msg: "Test from post" });
+    const { name, email, password } = body;
+
+    try {
+        if (!name) {
+            return <any> res
+                .status(403)
+                .json({ msg: "Debe colocar un nombre de usuario" });
+        }
+
+        if(!email) {
+            return <any> res.status(403).json({msg: "Debe colocar un correo eléctronico"})
+        }
+        
+        if(!password) {
+            return <any> res.status(403).json({msg: "Debe colocar una contraseña"})
+        }
+ 
+        const newUser = await registerUser({name, email, password});
+        res.status(200).json(newUser);
+    } catch (e) {
+        handleHttp(res, "Oops!! ocurrio un error", e);
+    }
 };
