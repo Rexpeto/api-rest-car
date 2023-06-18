01c9ed858d05722719bc70b92b43ef1432b1dedf
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-17 20:29:58 -0400

    Business logic of ctrl car

---
+++ src/services/car.service.ts
@@ -0,0 +1,42 @@
+import clientAxios from "../config/axios";
+import CarModel from "../models/car.schema";
+import { CarProps } from "../types/car.types";
+
+/* Get all cars of MongoDB
+ * @returns Array cars
+ * */
+export const getCarService = async () => {
+    const cars = await CarModel.find();
+
+    if (!cars.length) {
+        return { msg: "No se encontró resultados" };
+    }
+
+    return cars;
+};
+
+/* Search car from params search
+ * @params search {string}
+ * @returns
+ * */
+export const searchCarMake = async (make: string) => {
+    try {
+        const makeCar = await CarModel.find({ make });
+
+        console.log(process.env.RAPIDAPI_KEY);
+
+        if (!makeCar.length) {
+            const { data } = await clientAxios(`?make=${make}&limit=30`);
+
+            if (!data.length) return { msg: "Marca no encontrada" };
+
+            const newCars = await CarModel.create(data);
+            console.log(newCars);
+            return newCars;
+        }
+
+        return makeCar;
+    } catch (e) {
+        console.log(e);
+    }
+};
