bf8eed44c5ab416414a75d51aade30930b85e5f9
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-20 13:12:37 -0400

    ctrl of search car model

--- src/controllers/car.ctrl.ts
+++ src/controllers/car.ctrl.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import handleHttp from "../utils/error.handle";
-import { getCarService, searchCarMake } from "../services/car.service";
+import {
+    getCarService,
+    searchCarMake,
+    searchCarModel
+} from "../services/car.service";
 
 export const getCars = async (req: Request, res: Response) => {
     try {
@@ -20,5 +24,21 @@ export const searchMake = async ({ body }: Request, res: Response) => {
         res.status(200).json(cars);
     } catch (e) {
         handleHttp(res, "Oops! Ocurrió un error", e);
+    }
+};
+
+export const searchModel = async ({ body }: Request, res: Response) => {
+    const { model } = body;
+
+    if (!model) {
+        res.status(403).json({ msg: "Debe enviar un modelo" });
+    }
+
+    try {
+        const cars = await searchCarModel(model);
+
+        res.status(200).json(cars);
+    } catch (e) {
+        handleHttp(res, "Opps! Ocurrió un error", e);
     }
 };
